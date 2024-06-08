774a998e6e206499e1ba57ee27d1383586f4ca42
João Vinícius Costa <161096439+joaovincosta@users.noreply.github.com>
2024-06-07 22:39:25 -0300

    Add files via upload

---
+++ JQUERY/script.js
@@ -0,0 +1,103 @@
+$(Document).ready (() => {
+    
+    /* console.log($(".listateste li").eq(3).text())
+
+    $('.listateste li').each(index, item) => {
+        console.log(index)
+    }
+
+    $('.listateste li').eq(2).css({
+        "background-color" : "FF000",
+        "color": "#fff",
+        "padding": "10px",
+        "border-radius": "5px",
+        "margin-botton": "5px",
+    }) */
+
+    /* $(".insira-me").html("OLÁ MUNDO")
+    $(".lista-teste").prepend("<li>OK</>")
+    $(".lista-teste").prependTo("<li>OK</>")
+    $(".lista-teste").append("<li>OK</>")
+    $(".lista-teste").appendTo("<li>OK</>") */
+
+    /* $(".ativo").after("<i> Bla bla bla, bla </li>")
+    $(".ativo").before("<i> Bla bla bla, bla </li>")
+    $(".lista-teste li").eq(1).after("<i> Bla bla bla, bla </li>")
+    $(".lista-teste li").eq(1).before("<i> Bla bla bla, bla </li>")
+    $(".lista-teste li").eq(1).insertAfter("<i> Bla bla bla, bla </li>")
+    $(".lista-teste li").eq(1).insertBefore("<i> Bla bla bla, bla </li>") */
+
+    /* $(".lista-teste li.ativo").next().css("Atualizou")
+    console.log($(".lista-teste li.ativo ").next()) */
+
+    /* $(".lista-teste li.ativo").each((index, item) => {
+        console.log($(item).find(".teste"))
+    }) */
+
+    /* $(".lista-teste li.ativo").each((index, item) => {
+        console.log($(item).parent().parent().parent())
+    }) */
+
+    /* $(".lista-teste li.ativo").each((index, item) => {
+        console.log($(item).closet(".lista-teste").find("li").eq(0).text("Salve"))
+    }) */
+
+    /* $(".lista-teste li.ativo").each((index, item) => {
+        $(item).find(".teste").removeClass("teste")
+    }) */
+
+   /*  $(".lista-teste li.ativo").each((index, item) => {
+        $(item).find(".teste").hasClass("teste")
+    }) */
+
+    /* console.log($(".lista-teste li.ativo").attr('data-id'))
+    $(".lista-teste li.ativo").attr('data-id', "7890")
+    $(".lista-teste li.ativo").removeAttr('data-id', '1234') */
+
+    /* $(".insira-me").wrap("<div class='pai'></div>") */
+    /* $("li.ativo").wrapAll("<div class='pai'></div>") */
+    /* $("li.ativo").wrapInner("<div class='pai'></div>") */
+
+    /* $(".botao-teste").click((e) => {
+        console.log("Ok")
+    }) */
+
+    /* $(".link-teste").click((e) => {
+        e.preventDefault();
+        $("li").toggleClass("teste")
+    }) */
+
+    /* $(".link-teste").hover((e) => {
+        alert("Ops, seu mouse encostou em mim")
+    }) */
+
+    /* $("#input-teste").change((e) => {
+        console.log(e.target.test.value)
+    }) */
+
+    /* $("#input-teste").on("keyup keydown",(e) => {
+        console.log(e.target.test.value)
+    }) */
+
+    /* $("#botao-teste").click((e) => {
+        $(".insira-me").fadeIn(2000).done
+    }) */
+
+    /* $("#botao-teste").click((e) => {
+        $(".insira-me").fadeOut("slow")
+    }) */
+
+    /* $("#botao-teste").click((e) => {
+        $(".insira-me").hide()
+    }) */
+
+    /* $("#botao-teste").click((e) => {
+        $(".insira-me").show("slow")
+    }) */
+
+    /* $(".formulario-teste").submit((e) => {
+        e.preventDefault();
+        console.log($(e.target).serializeArray())
+    }) */
+
+})
